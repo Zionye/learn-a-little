735fab689886d758e00a28c96cf763693c716669
ruanm <247987314@qq.com>
2023-11-30 14:20:30 +0800

    bugfix：解决ts的prisma定义问题

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,36 @@
+// import { PrismaClient } from '@prisma/client';
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   console.log('有进来吗')
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
 import { PrismaClient } from '@prisma/client';
+
+// 声明 global 对象的属性类型
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma?: PrismaClient;
+    }
+  }
+}
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  console.log('有进来吗');
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
