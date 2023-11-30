31253554dfc2a7b0795b65112649b6fe73171a75
ruanm <247987314@qq.com>
2023-11-30 17:24:06 +0800

    bugfix：解决部署问题

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,9 +1,9 @@
-import prisma from '~/lib/prismadb'
-import type { GetStaticProps, NextApiRequest, NextApiResponse } from "next";
+// import prisma from '~/lib/prismadb'
+// import type { GetStaticProps, NextApiRequest, NextApiResponse } from "next";
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const user = await prisma.user.findMany() 
-  console.log('user: ', user);
+// export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+//   const user = await prisma.user.findMany() 
+//   console.log('user: ', user);
 
-  res.status(200).json({ status: 1 });
-}
+//   res.status(200).json({ status: 1 });
+// }
