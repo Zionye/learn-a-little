543b89c01d1759d53f6bb17ebcac68e51868442a
ruanm <247987314@qq.com>
2023-12-11 14:21:24 +0800

    fix: 根据不同环境配置 evn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const Dotenv = require("dotenv-webpack");
+
+const nextConfig = {
+  webpackConfig: {
+    plugins: [
+      new Dotenv({
+        // 指定要加载的 .env 文件
+        // Development 模式下加载 .env.development
+        // Production 模式下加载 .env.production
+        path:
+          process.env.NODE_ENV === "production"
+            ? ".env.production"
+            : ".env.development",
+      }),
+    ],
+  },
+};
+
+module.exports = nextConfig;
