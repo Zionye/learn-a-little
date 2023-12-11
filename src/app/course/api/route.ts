072b03fa50dc8cf73046dd9084321d926905f989
ruanm <247987314@qq.com>
2023-12-11 13:17:34 +0800

    style: 修改样式

--- src/app/course/api/route.ts
+++ src/app/course/api/route.ts
@@ -9,3 +9,13 @@ export async function GET() {
   });
   return Response.json({ status: 1, data: course })
 }
+
+export async function POST(request: Request){
+  const { title } = await request.json()
+  const course = await prisma.course.create({
+    data: {
+      title: title
+    }
+  })
+  return Response.json({ status: 1, data: course })
+}
--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import CourseCard from '@/components/CourseCard';
+import dotenv from "dotenv"
+import path from 'path';
+
+const isDev = process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
+if (isDev) {
+  dotenv.config({
+    path: path.resolve(__dirname, "../.env.development"),
+    override: true,
+  });
+} else if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: path.resolve(__dirname, "../.env.production") });
+} else {
+  console.error(`无效的 NODE_ENV:${process.env.NODE_ENV}`);
+}
 
 interface Course{
   id: string;
@@ -7,7 +21,8 @@ interface Course{
 }
 
 async function fetchCourses(): Promise<Course[]> {
-  const response = await fetch(`http://localhost:3000/course/api`);
+  const basePath = process.env.BASE_PATH;
+  const response = await fetch(`${basePath}/course/api`);
  
   if (!response.ok) {
     throw new Error('Failed to fetch data')
